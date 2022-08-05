d870f7055652585cc50b3ec7f573f0c138c3a304
Ernesto Garcia <gernesto14@yahoo.com>
2022-08-04 22:09:32 -0400

    this is a HellowWorld py program uploading from IntellyJ to Github

--- IO Basic Files.py
+++ IO Basic Files.py
@@ -1,44 +1,23 @@
 #Importing OS module
 import os
 
-#
-# # my file = open('test.txt')
-# print('\nPrint current working directory')
+
 # print(os.getcwd())
-#
-# print('\n')
-# #Changing directory path
-# os.chdir('/Users/Ernesto Garcia/Documents/Programming/Python/Class/')
-#
-# print('\n')
-# #List all files in that folder
-# print(os.listdir())
-#
-# #Create new file
-# # os.mkdir('New Folder Test')
-# #Delete folder/file
-# #os.rmdir('New Folder Test')
-
-# os.chdir('/Users/Ernesto Garcia/Documents/Programming/Python/Class/Udemy Course by Portilla')
-# print('\n')
-# print('List of all files: \n')
-# #List all files in that folder
-# print(os.listdir())
-#
-# myfile = open('\\Users\\Ernesto Garcia\\Documents\\'
-#               'Programming\\Python\\Class\\Udemy Course by Portilla\\test.txt')
-#
-# myfile = open('test.txt', 'r')
-#
-#
-#
-# myfile = open('test.txt', mode = 'r')
-#
-
-
-print(os.getcwd())
 print(os.listdir())
 
-myfile = open('test1', 'r', encoding='cp1252')
 
-print(myfile)
+# addnote = "This is my first line"
+# myfile.write(addnote)
+
+userinput = True
+while userinput is not False:
+    userinput = input("\nEnter operation:\n "
+                      "\t'0' ends loop"
+                      "\t'1' continue ")
+
+    if userinput == '0':
+        break
+
+
+
+
