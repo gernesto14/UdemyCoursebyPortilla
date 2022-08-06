bb2ab91fd96491eaa87b3a5b67ffd65ec58dbb0f
Ernesto Garcia <gernesto14@yahoo.com>
2022-08-05 21:22:57 -0400

    this is a HellowWorld py program uploading from IntellyJ to Github

--- IO Basic Files.py
+++ IO Basic Files.py
@@ -1,9 +1,7 @@
-#Importing OS module
+# Importing OS module
 import os
 
-
-print(os.listdir()) #Print directory list files
-
+print(os.listdir())  # Print directory list files
 
 userinput = True
 count = 0
@@ -11,37 +9,26 @@ while userinput is not False:
 
     newline = "{} line\t".format(count)
     userinput = str(input("\nEnter operation:\n "
-                      "\t'0' ends "
-                      "\t'1' continue "
-                      "\t'W' write new line" + str(count) + \
-                      "\t'R' read\t"
-                      "\t'A' append"))
+                          "\t'0' ends "
+                          "\t'1' continue "
+                          "\t'W' write new line" + str(count) + \
+                          "\t'R' read\t"
+                          "\t'A' append"))
 
     if userinput == '0':
         break
     elif userinput == 'w':
-        with open('test.txt', mode= 'w+') as f:
+        with open('test.txt', mode='w+') as f:
             contents = f.write('\n' + newline)
             print(contents)
 
     elif userinput == 'r':
-        with open('test.txt', mode= 'r') as f:
+        with open('test.txt', mode='r') as f:
             contents = f.read()
         print(contents)
 
     elif userinput == 'a':
-        with open('test.txt', mode= 'a') as f:
+        with open('test.txt', mode='a') as f:
             contents = f.write('\t' + newline)
             print(contents)
     count = count + 1
-
-
-
-
-
-
-
-
-
-
-
