ad5cacf3cc0f7d605af3c7d4541ce6ef0bfb6c73
Ernesto Garcia <gernesto14@yahoo.com>
2022-08-05 20:37:18 -0400

    this is a HellowWorld py program uploading from IntellyJ to Github

--- IO Basic Files.py
+++ IO Basic Files.py
@@ -2,24 +2,45 @@
 import os
 
 
-# print(os.getcwd())
-print(os.listdir())
+print(os.listdir()) #Print directory list files
 
-
-# addnote = "This is my first line"
-# myfile.write(addnote)
 
 userinput = True
+count = 0
 while userinput is not False:
-    userinput = input("\nEnter operation:\n "
-                      "\t'0' ends loop"
-                      "\t'1' continue ")
 
-
-
+    newline = "{} line\t".format(count)
+    userinput = str(input("\nEnter operation:\n "
+                      "\t'0' ends "
+                      "\t'1' continue "
+                      "\t'W' write new line" + str(count) + \
+                      "\t'R' read\t"
+                      "\t'A' append"))
 
     if userinput == '0':
         break
+    elif userinput == 'w':
+        with open('test.txt', mode= 'w+') as f:
+            contents = f.write('\n' + newline)
+            print(contents)
+
+    elif userinput == 'r':
+        with open('test.txt', mode= 'r') as f:
+            contents = f.read()
+        print(contents)
+
+    elif userinput == 'a':
+        with open('test.txt', mode= 'a') as f:
+            contents = f.write('\t' + newline)
+            print(contents)
+    count = count + 1
+
+
+
+
+
+
+
 
 
 
