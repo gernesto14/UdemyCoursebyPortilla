aa60a088adf56a85639275e7ad2acaaffc58c467
Ernesto G <96400222+gernesto14@users.noreply.github.com>
2022-08-21 15:11:43 -0400

    function and methods homework

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,24 +2,5 @@
 
 print('Hello World')  # IntellyJ
 
-def paper_doll(text):
-    mylist = []
-    newlist = []
-    i = 0
-    for x in text:
-        mylist.append(text[i])
-        i = i + 1
-    print(mylist)
-
-    i = 0
-    while i <= (len(mylist)):
-        if x != True:
-            newlist.append(mylist[i]*3)
-        else:
-            print('Breaking loop')
-            break
-        i = i +1
-    print(newlist)
-
-
-paper_doll('Hel')
+some_digit = '100'
+some_digit.isdigit()
